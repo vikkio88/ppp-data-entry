540e7408d891fdc0648e32d1b9c8b50d3fafffc1
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2025-09-22 20:37:40 +0100

    forget better episode meta

--- src/libs/types.ts
+++ src/libs/types.ts
@@ -2,24 +2,30 @@ export type Author = "Lorro" | "Nick" | "Sio" | string;
 
 export type VotoPizze = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10;
 
+export type Timestamp = { hours: number; minutes: number; seconds: number };
+
+export type BaseTopic = {
+  author: Author;
+  description: string;
+  tags: string;
+  link?: string;
+  timestamp?: Timestamp;
+};
+export type Pizza = {
+  fette: VotoPizze;
+  descrizione?: string;
+};
+
 export type Episode = {
   number: number;
   date?: Date;
-  menews: { author: Author; topic: string; link?: string }[];
-  lorrowap: { author: Author; topic: string; link?: string }[];
-  main: {
-    author: Author;
-    topic: string;
-    link?: string;
-    pizza: {
-      fette: VotoPizze;
-      descrizione?: string;
-    };
-  }[];
-  dolcetto: { author: Author; topic: string; link?: string }[];
-  amaro: { author: Author; topic: string; link?: string }[];
-  lore: { author: Author; description: string }[];
-  dieciPizze: { author: Author; description: string }[];
+  menews: BaseTopic[];
+  lorrowap: BaseTopic[];
+  main: BaseTopic & { pizza: Pizza }[];
+  dolcetto: BaseTopic[];
+  amaro: BaseTopic[];
+  lore: BaseTopic[];
+  others: BaseTopic[];
   meta: {
     user: string;
     date: Date;
